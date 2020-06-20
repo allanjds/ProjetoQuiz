dc204bd7505c8b7d0b863a70d644ee52165b5d54
AnnaHermenegildo <54596765+AnnaHermenegildo@users.noreply.github.com>
2020-06-20 13:56:37 -0300

    Criação do Ranking no pacote java

---
+++ src/java/model/Ranking.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Anna
+ */
+public class Ranking {
+    
+    private static ArrayList<Ranking> ranks = null;
+
+    private String user;
+    private double result;
+    private int id;
+
+    public Ranking(String user, double result, int id) {
+        this.user = user;
+        this.result = result;
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static ArrayList<Ranking> orderByResult(ArrayList<Ranking> rankings) {
+        Collections.sort(rankings, (Ranking r1, Ranking r2) -> {
+            if (r1.getResult() > r2.getResult()) {
+                return -1;
+            } else if (r1.getResult() == r2.getResult()) {
+                return 0;
+            } else {
+                return 1;
+            }
+        });
+        return rankings;
+    }
+
+    public static ArrayList<Ranking> orderById(ArrayList<Ranking> rankings) {
+        Collections.sort(rankings, (Ranking r1, Ranking r2) -> {
+            if (r1.getId() < r2.getId()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        return rankings;
+    }
+  public static ArrayList<Ranking> getRanking() {
+        if (ranks == null) {
+            ranks = new ArrayList<>();
+            ranks.add(new Ranking("Anna", 10.0, 0));
+        }
+
+        return ranks;
+    }
+}
--- web/home.jsp
+++ web/home.jsp
@@ -1,3 +1,7 @@
+<%-- 
+    Author     : Anna
+--%>
+<%@page import="model.Ranking"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -7,10 +11,20 @@
     </head>
     <body>
         <%@include file="WEB-INF/jspf/menu.jspf" %>
-        <h2> Bem vindo <%=session.getAttribute("user.login")%> </h2>
-        
         
         <h2>Clique <a href="quiz.jsp">aqui</a> para iniciar seu teste de conhecimento</h2>
+        
+                <table>
+            <h3>Ultimos Resultados</h3>
+            <tbody><%
+                for (Ranking r : Ranking.orderById(Ranking.getRanking())) {%>
+                <tr>
+                    <td><%= r.getUser()%></td>
+                    <td><%= r.getResult()%></td>
+                </tr>
+                <% }%>
+            </tbody>
+        </table>
     </body>
 </html>
 
