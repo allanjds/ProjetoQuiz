09e22f31057a00df188a83f0c7f0ec3e9ea42ba3
AnnaHermenegildo <54596765+AnnaHermenegildo@users.noreply.github.com>
2020-06-22 20:11:38 -0300

    Exclusões de alterações

--- src/java/model/Questao.java
+++ src/java/model/Questao.java
@@ -5,46 +5,10 @@
  */
 package model;
 
-import database.DbConfig;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-
 /**
  *
- * @author Gabriel
+ * @author Anna
  */
 public class Questao {
-    private String enunciated;
-
-    public static ArrayList<Questao> listaQuestoes()throws Exception{
-          ArrayList<Questao> list = new ArrayList<>();
-          Class.forName(DbConfig.CLASS_NAME);
-          Connection con = DriverManager.getConnection(DbConfig.URL);
-          Statement stmt = con.createStatement();
-          ResultSet rs = stmt.executeQuery("SELECT * from questions");
-          while(rs.next()){
-              list.add(new Questao(rs.getString("enunciated")));
-          }
-          rs.close();
-          stmt.close();
-          con.close();
-          return list;
-    }
-
-    public Questao(String enunciated) {
-        this.enunciated = enunciated;
-    }
-
-    public String getEnunciated() {
-        return enunciated;
-    }
-
-    public void setEnunciated(String enunciated) {
-        this.enunciated = enunciated;
-    }
-
     
 }
